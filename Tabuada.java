32a3fae2314861b558ceee4b47df4dd29a9834b9
leandrocrf <lemottajr@hotmail.com>
2021-02-26 04:52:35 -0300

    Tabuada - semana 1

---
+++ Tabuada.java
@@ -0,0 +1,16 @@
+/* Leandro Motta Junior
+ * 0050014733
+ */
+
+package First;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(10 + "x" + i + "=" + (10 * i));
+		}
+
+	}
+
+}
