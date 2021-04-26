4ab4b4ff1b575f09ff703219791231488e22f66e
leandrocrf <lemottajr@hotmail.com>
2021-04-26 17:55:47 -0300

    Implementando getters and setters e incluindo enunciado

--- LeandroAv1OOP/questao2/Iandroid.java
+++ LeandroAv1OOP/questao2/Iandroid.java
@@ -1,11 +1,29 @@
+/* 2.2 - Crie uma classe IAndroid que herda de CellPhone e implementa os métodos abaixo:
+- mostrar_marca() - deve exibir a "marca IPhone"
+*/
+
 package LeandroAv1OOP.questao2;
 
 public class Iandroid extends Cellphone {
 
+    private String marca;
+
+    //Usando Getters and Setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
     //Método da classe IAndroid
 
     public void mostrar_marca(){
-        System.out.println("Iphone");
+
+        System.out.println("Marca: "+ this.marca);
+
+        // System.out.println("Iphone");
     }
 
     //Métodos implementados da classe CellPhone
