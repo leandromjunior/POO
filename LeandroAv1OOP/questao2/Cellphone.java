2978ad93d9b65a3e26a2b1d276502be5641744bf
leandrocrf <lemottajr@hotmail.com>
2021-04-26 17:54:47 -0300

    Incluindo enunciado

--- LeandroAv1OOP/questao2/Cellphone.java
+++ LeandroAv1OOP/questao2/Cellphone.java
@@ -1,3 +1,10 @@
+/* 2.1 - Crie uma classe abstrata chamada CellPhone com os seguintes atributos e métodos:
+- atributo privado booleano ligado
+- metodo publico ligar()
+- metodo público desligar()
+- programe os métodos para colocar verdadeiro ou falso no flag ligado
+*/
+
 package LeandroAv1OOP.questao2;
 
 abstract class Cellphone {
