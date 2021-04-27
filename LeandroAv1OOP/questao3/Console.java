335d9a2cc620a36068357d1490431eae2fea7125
leandrocrf <lemottajr@hotmail.com>
2021-04-27 17:57:51 -0300

    questao 3 simulado - Superclasse

---
+++ LeandroAv1OOP/questao3/Console.java
@@ -0,0 +1,20 @@
+/* Crie 2 classes tema livre.
+3.1 - A segunda classe deve herdar da primeira classe
+3.2 - Crie um atributo privado na primeira classe e seus métodos getter/setter
+*/
+
+//package LeandroAv1OOP.questao3;
+
+public class Console{
+
+    private int modelo;
+
+    //getters and setters
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+}
