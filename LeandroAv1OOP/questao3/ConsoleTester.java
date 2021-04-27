24a953c1b8c1324e02c8bd7311afe4b507b22d78
leandrocrf <lemottajr@hotmail.com>
2021-04-27 17:59:03 -0300

    questao 3 simulado - Tester

---
+++ LeandroAv1OOP/questao3/ConsoleTester.java
@@ -0,0 +1,19 @@
+// 3.4 - Crie uma Classe Tester para testar a segunda classe
+
+//package LeandroAv1OOP.questao3;
+
+public class ConsoleTester {
+    
+    public static void main(String[] args) {
+        
+        Playstation ps = new Playstation();
+
+        //Atribuindo valor à variavel 
+        ps.setModelo(5);
+        //ps.setModelo(4);
+        //ps.setModelo(3);
+
+        //Chamando método
+        ps.especificacao();
+    }
+}
